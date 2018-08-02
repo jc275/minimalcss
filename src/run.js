a5da623cf5866d48c0cab7690ae6e8e0a9f32065
jc275 <jc275@users.noreply.github.com>
2018-08-02 14:11:02 +0100

    Strip url fragments from stylesheet hrefs

--- src/run.js
+++ src/run.js
@@ -312,7 +312,10 @@ const processPage = ({
             link.media !== 'print' &&
             !link.href.toLowerCase().startsWith('data:')
           ) {
-            hrefs.push(link.href);
+            // Fragments are omitted from puppeteer's response.url(),
+            // so we also need to strip them here, otherwise the hrefs
+            // won't always match when we check for missing ASTs.
+            hrefs.push(link.href.replace(/#.*$/, ''));
           }
         });
         return {
